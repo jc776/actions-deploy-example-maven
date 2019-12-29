eeb9b364ef5b63f451e02e3c37a59a1a6c97cc83
Jack Coady <jack.coady@cantab.net>
2019-12-29 09:25:19 +0000

    single java class

---
+++ src/main/java/io/github/jc776/maven/Example.java
@@ -0,0 +1,18 @@
+package io.github.jc776.maven;
+
+public class Example {
+
+	private boolean enabled;
+
+	public Example() {
+		enabled = true;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+}
